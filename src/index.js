6ee2779ba274f6bf532290670b6941084b973f80
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-08-31 11:20:51 +0300

    Add image and title

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-// write your code here
+document.addEventListener('DOMContentLoaded', () => {
+    const image = document.getElementById('card-image'),
+    h2 = document.getElementById('card-title')
+    
+    fetch("http://localhost:3000/images/1")
+    .then(response => response.json())
+    .then(obj => {
+        image.removeAttribute('src')
+        image.setAttribute('src', `${obj.image}`)
+        h2.innerText = `${obj.title}`
+    })
+})
