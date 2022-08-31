00bdcc4a43ecc94c727b58900d7d7425b2c45dc2
Patrick-Njiru <patrick.njiru@student.moringaschool.com>
2022-08-31 12:25:32 +0300

    Create new variables and replace the comments in html with comments from server.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     const image = document.getElementById('card-image'),
-    h2 = document.getElementById('card-title')
+    h2 = document.getElementById('card-title'),
+    form = document.getElementById('comment-form'),
+    comments = document.getElementById('comments-list')
     
     fetch("http://localhost:3000/images/1")
     .then(response => response.json())
@@ -9,4 +11,17 @@ document.addEventListener('DOMContentLoaded', () => {
         image.setAttribute('src', `${obj.image}`)
         h2.innerText = `${obj.title}`
     })
+    .catch(error => error.message)
+
+    fetch("http://localhost:3000/comments")
+    .then(response => response.json())
+    .then(object => {
+        comments.innerHTML = 
+        `
+        <li> ${object[0].content}</li>
+        <li> ${object[1].content}</li>       
+        <li> ${object[2].content}</li>             
+        `
+     })
+    .catch(error => error.message)
 })
